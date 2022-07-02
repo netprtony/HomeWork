77ecd9aae5cb6426e43322e58b9142a1e05c7ef3
netprtony <huynhvikhang913@gmail.com>
2022-07-02 14:28:08 +0700

    save

--- tree.cpp
+++ tree.cpp
@@ -17,7 +17,6 @@ TNode* Create(ItemType x){
     if(!p) return NULL;
     p->data = x;
     p->right = p->left = NULL;
-    delete p;
     return p;
 }
 void showTNode(TNode *p){
@@ -59,13 +58,25 @@ void createArray(Btree &bt, ItemType *a, int n){
     initBtree(bt);
     for (int i = 0; i < n; i++)
     {
-        TNode* p = Create(a[i]);
+        TNode* p = Create(a[i]) ;
+        insert(bt.Root, p);
+    }
+}
+void createHandMake(Btree &bt, int n){
+    initBtree(bt);
+    ItemType x; 
+    printf("How many tree do you want : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("new X : "); scanf("%d", &x);
+        TNode* p = Create(x);
         insert(bt.Root, p);
     }
 }
 void createAuto(Btree &bt){
     int n;
-    ItemType x;
+     ItemType x;
     printf("How many tree do you want : ");
     scanf("%d", &n);
     initBtree(bt);
@@ -130,14 +141,6 @@ TNode* FindTNode2(TNode* root, ItemType x){
     }
     return NULL;
 }
-void menu(){
-    printf("\n****************MENU***********************");
-    printf("\n*1.Create Tree form array.                *");
-    printf("\n*2.Show NLR.                              *");
-    printf("\n*3.Find node any.                         *");
-    printf("\n*0.Exit.                                  *");
-    printf("\n*******************************************");
-}
 bool isLeaf(TNode* t){
     if(!t) return false; 
     if(t->left || t->right) return false;//là lá khi và chỉ khi không có nhánh con chỉ có nhánh cha.
@@ -163,29 +166,45 @@ bool deleteTNoderight(TNode* t, ItemType &x){
     delete p;
     return true;
 }
+void menu(){
+    printf("\n****************MENU***********************");
+    printf("\n*1.Create Tree form array.                *");
+    printf("\n*2.Create Tree random.                    *");
+    printf("\n*3.Create Tree hand make.                 *");
+    printf("\n*4.Show NLR.                              *");
+    printf("\n*5.Find node any.                         *");
+    printf("\n*0.Exit.                                  *");
+    printf("\n*******************************************");
+}
 void process(){
     int selectFunction;
     Btree bt;
     TNode* p;
     ItemType x;
-    ItemType a[] = {10, 2,5,4,65,6,3,25,6,36,4};
-    int n =12;
+    ItemType a[] = {10, 2, 5, 4, 65, 6, 3, 25, 6, 36, 4};
+    int n = 12;
     initBtree(bt);
     do
     {
         menu();
         printf("\nSelection : "); scanf("%d", &selectFunction);
+        printf("\n----------RESULT---------------\n");
         switch (selectFunction)
         {
-        case 1 :    createAuto(bt);  
+        case 1 :    createArray(bt, a, n);  
+                    break;    
+        case 2 :    createAuto(bt);  
                     break;
-        case 2 : RNL(bt.Root);
+        case 3 :    createHandMake(bt, n);  
+                    break;           
+        case 4 : RNL(bt.Root);
                 break;
-        case 3 : printf("X = "); scanf("%d", &x);
-                if(FindTNode(bt.Root, x)) printf("Node %d in tree.", x);
+        case 5 : printf("X = "); scanf("%d", &x);
+                if(FindTNode(bt.Root, x)!= NULL) printf("Node %d in tree.", x);
                 else printf("Node %d is not in tree");
                 break; 
         }
+        printf("\n--------------------------------");
     } while (selectFunction );
     
 }
